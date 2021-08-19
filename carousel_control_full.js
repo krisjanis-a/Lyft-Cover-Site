1d752db0cfe0619a530b877da3dc583afab3da9c
krisjanis-a <krishjanis96@gmail.com>
2021-08-19 14:48:49 +0300

    responsive css fixed for sections 1,2,5,6 before changing carousel.js

--- carousel_control_full.js
+++ carousel_control_full.js
@@ -1,6 +1,5 @@
-
 const slider = document.querySelector("#carousel-container__items");
-const carousel = document.querySelector("#carousel-container__items-wrapper")
+const carousel = document.querySelector("#carousel-container__items-wrapper");
 
 const rightButton = document.querySelector("#carousel__right-button");
 const leftButton = document.querySelector("#carousel__left-button");
@@ -10,234 +9,282 @@ const buttonItem2 = document.querySelector("#dot-2");
 const buttonItem3 = document.querySelector("#dot-3");
 const buttonItem4 = document.querySelector("#dot-4");
 
+// CHECK VIEWPORT SIZE AND DETERMINE OFFSET FOR CAROUSEL
+
 let offset = 12.5;
+
+let offsetBelow1000px = 0;
+
+// window.onload = changesOnLoad;
+
+window.onresize = changesOnResize;
+
+// function changesOnLoad() {
+//   // if (window.innerWidth <= 1000) {
+//   //   offset = 12.5;
+//   // }
+//   // if (window.innerWidth <= 1000) {
+//   //   offset = 100;
+//   // }
+//   console.log(
+//     "innerwidth on load: " + window.innerWidth + "; offset: " + offset
+//   );
+// }
+
+function changesOnResize() {
+  offsetBelow1000px = 37.5;
+
+  // if (window.innerWidth > 1000) {
+  //   console.log("innerwidth larger than 1000px");
+  //   offset = 12.5;
+  // }
+  // if (window.innerWidth <= 1000) {
+  //   console.log("innerwidth smaller or equal to 1000px");
+  //   offset = 100;
+  // }
+}
+
+// ACTIVE SECTION INDEX => BEGINS FROM 0 !!! Items and corresponding indices:
+// ITEM-3 - 0 | ITEM-4 - 1 | ITEM-1 - 2 | ITEM-2 - 3 | ITEM-3 - 4 | | ITEM-4 - 5 | ITEM-1 - 6 | ITEM-2 - 7
 
 var activeSectionIndex = 3;
 
-leftButton.addEventListener("click", function(){
+// BUTTON TO MOVE CAROUSEL TO LEFT
 
-    // console.log("left");
+leftButton.addEventListener("click", function () {
+  console.log("left");
 
-    activeSectionIndex -= 1;
+  activeSectionIndex -= 1;
 
-    slider.style.transform = "translate(" + ( - offset * (activeSectionIndex-1)) + "%)";   
+  slider.style.transform =
+    "translate(" + -offset * (activeSectionIndex - 1) + "%)";
 
-    console.log("calculated absolute offset: " + ( - offset * (activeSectionIndex-1)));
-    console.log("active section index: " + activeSectionIndex);
+  console.log(
+    "calculated absolute offset: " + -offset * (activeSectionIndex - 1)
+  );
+  console.log("active section index: " + activeSectionIndex);
 
-    slider.addEventListener("transitionstart", disableAllButtons);
+  slider.addEventListener("transitionstart", disableAllButtons);
 
-    if(activeSectionIndex === 6){
+  if (activeSectionIndex === 6) {
+    slider.addEventListener("transitionend", rollBackwards);
+  }
 
-        slider.addEventListener("transitionend", rollBackwards);
-    }
+  if (activeSectionIndex === 1) {
+    slider.addEventListener("transitionend", rollForwards);
+  }
 
-    if(activeSectionIndex === 1){
+  setActiveSection();
+  setActiveDot();
 
-        slider.addEventListener("transitionend", rollForwards);
-    }
-
-    setActiveSection();
-    setActiveDot();
-
-    slider.addEventListener("transitionend", enableAllButtons);
+  slider.addEventListener("transitionend", enableAllButtons);
 });
 
-rightButton.addEventListener("click", function(){
-    
-    // console.log("right");
+// BUTTON TO MOVE CAROUSEL TO RIGHT
 
-    activeSectionIndex += 1;
+rightButton.addEventListener("click", function () {
+  console.log("right");
 
-    slider.style.transform = "translate(" + ( - offset * (activeSectionIndex-1)) + "%)";
+  activeSectionIndex += 1;
 
-    console.log("calculated absolute offset: " + ( - offset * (activeSectionIndex-1)));
-    console.log("active section index: " + activeSectionIndex);
+  slider.style.transform =
+    "translate(" + -offset * (activeSectionIndex - 1) + "%)";
 
-    slider.addEventListener("transitionstart", disableAllButtons);
+  console.log(
+    "calculated absolute offset: " + -offset * (activeSectionIndex - 1)
+  );
+  console.log("active section index: " + activeSectionIndex);
 
-    if(activeSectionIndex === 6){
+  slider.addEventListener("transitionstart", disableAllButtons);
 
-        slider.addEventListener("transitionend", rollBackwards);
-    }
+  if (activeSectionIndex === 6) {
+    slider.addEventListener("transitionend", rollBackwards);
+  }
 
-    if(activeSectionIndex === 1){
+  if (activeSectionIndex === 1) {
+    slider.addEventListener("transitionend", rollForwards);
+  }
 
-        slider.addEventListener("transitionend", rollForwards);
-    }
+  setActiveSection();
+  setActiveDot();
 
-    setActiveSection();
-    setActiveDot();
-
-    slider.addEventListener("transitionend", enableAllButtons);
+  slider.addEventListener("transitionend", enableAllButtons);
 });
+
+// WHEN DISPLAYING ITEM-4#5 AND CLICKING MOVE RIGHT (MOVE FROM ITEM-4#5 => ITEM-1#6) - JUMPS FROM ITEM-1#6 => ITEM-1#2 WITHOUT VISIBLE TRANSITION TO ENSURE INFINITE SCROLL
 
 function rollBackwards() {
-    
-    activeSectionIndex = 2;
-    console.log("active section index: " + activeSectionIndex);
+  console.log("roll backwards");
 
-    slider.style.transition = "none";
-    slider.style.transform = "translate(" + ( - offset * (activeSectionIndex-1)) + "%)";
+  activeSectionIndex = 2;
+  console.log("active section index: " + activeSectionIndex);
 
-    setTimeout(function(){
-        slider.style.transition = "transform 1s"
-    });
+  slider.style.transition = "none";
+  slider.style.transform =
+    "translate(" + -offset * (activeSectionIndex - 1) + "%)";
 
-    slider.removeEventListener("transitionend", rollBackwards);
-};
+  setTimeout(function () {
+    slider.style.transition = "transform 1s";
+  });
+
+  slider.removeEventListener("transitionend", rollBackwards);
+}
+
+// WHEN DISPLAYING ITEM-1#2 AND CLICKING MOVE LEFT (MOVE FROM ITEM-1#2 => ITEM-4#1) - JUMPS FROM ITEM-4#1 => ITEM-4#5 WITHOUT VISIBLE TRANSITION TO ENSURE INFINITE SCROLL
 
 function rollForwards() {
+  console.log("roll forwards");
 
-    activeSectionIndex = 5;
-    console.log("active section index: " + activeSectionIndex);
+  activeSectionIndex = 5;
+  console.log("active section index: " + activeSectionIndex);
 
-    slider.style.transition = "none";
-    slider.style.transform = "translate(" + ( - offset * (activeSectionIndex-1)) + "%)";
+  slider.style.transition = "none";
+  slider.style.transform =
+    "translate(" + -offset * (activeSectionIndex - 1) + "%)";
 
-    setTimeout(function(){
-        slider.style.transition = "transform 1s"
+  setTimeout(function () {
+    slider.style.transition = "transform 1s";
+  });
+
+  slider.removeEventListener("transitionend", rollForwards);
+}
+
+buttonItem1.addEventListener("click", function () {
+  activeSectionIndex = 2;
+
+  slider.style.transform =
+    "translate(" + -offset * (activeSectionIndex - 1) + "%)";
+  console.log("active section index: " + activeSectionIndex);
+
+  slider.addEventListener("transitionstart", disableAllButtons);
+  setActiveSection();
+  setActiveDot();
+  slider.addEventListener("transitionend", enableAllButtons);
+});
+
+buttonItem2.addEventListener("click", function () {
+  activeSectionIndex = 3;
+  console.log("active section index: " + activeSectionIndex);
+
+  slider.style.transform =
+    "translate(" + -offset * (activeSectionIndex - 1) + "%)";
+
+  slider.addEventListener("transitionstart", disableAllButtons);
+  setActiveSection();
+  setActiveDot();
+  slider.addEventListener("transitionend", enableAllButtons);
+});
+
+buttonItem3.addEventListener("click", function () {
+  activeSectionIndex = 4;
+  console.log("active section index: " + activeSectionIndex);
+
+  slider.style.transform =
+    "translate(" + -offset * (activeSectionIndex - 1) + "%)";
+
+  slider.addEventListener("transitionstart", disableAllButtons);
+  setActiveSection();
+  setActiveDot();
+  slider.addEventListener("transitionend", enableAllButtons);
+});
+
+buttonItem4.addEventListener("click", function () {
+  activeSectionIndex = 5;
+  console.log("active section index: " + activeSectionIndex);
+
+  slider.style.transform =
+    "translate(" + -offset * (activeSectionIndex - 1) + "%)";
+
+  slider.addEventListener("transitionstart", disableAllButtons);
+  setActiveSection();
+  setActiveDot();
+  slider.addEventListener("transitionend", enableAllButtons);
+});
+
+function setActiveDot() {
+  document
+    .querySelector("#controls__dots-container .item-active")
+    .classList.remove("item-active");
+
+  if (activeSectionIndex === 2 || activeSectionIndex === 6) {
+    buttonItem1.classList.add("item-active");
+  }
+
+  if (activeSectionIndex === 3) {
+    buttonItem2.classList.add("item-active");
+  }
+
+  if (activeSectionIndex === 4) {
+    buttonItem3.classList.add("item-active");
+  }
+
+  if (activeSectionIndex === 5 || activeSectionIndex === 1) {
+    buttonItem4.classList.add("item-active");
+  }
+}
+
+function setActiveSection() {
+  let activeSections = document.querySelectorAll(
+    "#carousel-container__items .item-active"
+  );
+
+  activeSections.forEach((section) => {
+    section.classList.remove("item-active");
+  });
+
+  if (activeSectionIndex === 2 || activeSectionIndex === 6) {
+    let item1sections = document.querySelectorAll(".carousel__item-1");
+
+    item1sections.forEach((item) => {
+      item.classList.add("item-active");
     });
+  }
 
-    slider.removeEventListener("transitionend", rollForwards);
-};
+  if (activeSectionIndex === 3) {
+    let item1sections = document.querySelectorAll(".carousel__item-2");
 
-buttonItem1.addEventListener("click", function(){ 
-
-    activeSectionIndex = 2;
-
-    slider.style.transform = "translate(" + ( - offset * (activeSectionIndex-1)) + "%)";
-    console.log("active section index: " + activeSectionIndex); 
-
-    slider.addEventListener("transitionstart", disableAllButtons);
-    setActiveSection();
-    setActiveDot();
-    slider.addEventListener("transitionend", enableAllButtons);
-});
-
-buttonItem2.addEventListener("click", function(){
-
-    activeSectionIndex = 3;
-    console.log("active section index: " + activeSectionIndex);
-
-    slider.style.transform = "translate(" + ( - offset * (activeSectionIndex-1)) + "%)"; 
-   
-    slider.addEventListener("transitionstart", disableAllButtons);
-    setActiveSection();
-    setActiveDot();
-    slider.addEventListener("transitionend", enableAllButtons);
-});
-
-buttonItem3.addEventListener("click", function(){  
-    
-    activeSectionIndex = 4;
-    console.log("active section index: " + activeSectionIndex);
-
-    slider.style.transform = "translate(" + ( - offset * (activeSectionIndex-1)) + "%)"; 
-
-    slider.addEventListener("transitionstart", disableAllButtons);
-    setActiveSection();
-    setActiveDot();
-    slider.addEventListener("transitionend", enableAllButtons);
-});
-
-buttonItem4.addEventListener("click", function(){
-
-    activeSectionIndex = 5;
-    console.log("active section index: " + activeSectionIndex);
-
-    slider.style.transform = "translate(" + ( - offset * (activeSectionIndex-1)) + "%)"; 
-
-    slider.addEventListener("transitionstart", disableAllButtons);
-    setActiveSection();
-    setActiveDot();
-    slider.addEventListener("transitionend", enableAllButtons);
-});
-
-function setActiveDot(){
-
-    document.querySelector('#controls__dots-container .item-active').classList.remove('item-active');
-
-    if(activeSectionIndex === 2 || activeSectionIndex === 6){
-        buttonItem1.classList.add('item-active');
-    }
-
-    if(activeSectionIndex === 3){
-        buttonItem2.classList.add('item-active');
-    }
-
-    if(activeSectionIndex === 4){
-        buttonItem3.classList.add('item-active');
-    }
-
-    if(activeSectionIndex === 5 || activeSectionIndex === 1){
-        buttonItem4.classList.add('item-active');
-    }
-
-}
-
-function setActiveSection(){
-
-    let activeSections = document.querySelectorAll('#carousel-container__items .item-active');
-    
-    activeSections.forEach(section => {
-        section.classList.remove('item-active'); 
+    item1sections.forEach((item) => {
+      item.classList.add("item-active");
     });
+  }
 
-    if(activeSectionIndex === 2 || activeSectionIndex === 6){
-        let item1sections = document.querySelectorAll('.carousel__item-1');
-        
-        item1sections.forEach(item => {
-            item.classList.add('item-active'); 
-        });
-    }
+  if (activeSectionIndex === 4) {
+    let item1sections = document.querySelectorAll(".carousel__item-3");
 
-    if(activeSectionIndex === 3){
-        let item1sections = document.querySelectorAll('.carousel__item-2');
-        
-        item1sections.forEach(item => {
-            item.classList.add('item-active'); 
-        });
-    }
+    item1sections.forEach((item) => {
+      item.classList.add("item-active");
+    });
+  }
 
-    if(activeSectionIndex === 4){
-        let item1sections = document.querySelectorAll('.carousel__item-3');
-        
-        item1sections.forEach(item => {
-            item.classList.add('item-active'); 
-        });
-    }
+  if (activeSectionIndex === 5 || activeSectionIndex === 1) {
+    let item1sections = document.querySelectorAll(".carousel__item-4");
 
-    if(activeSectionIndex === 5 || activeSectionIndex === 1){
-        let item1sections = document.querySelectorAll('.carousel__item-4');
-        
-        item1sections.forEach(item => {
-            item.classList.add('item-active'); 
-        });
-    }
-
+    item1sections.forEach((item) => {
+      item.classList.add("item-active");
+    });
+  }
 }
 
-function disableAllButtons(){
-    leftButton.disabled = true;
-    rightButton.disabled = true;
+function disableAllButtons() {
+  leftButton.disabled = true;
+  rightButton.disabled = true;
 
-    document.querySelector("#carousel__item-1-button").disabled = true;
-    document.querySelector("#carousel__item-2-button").disabled = true;
-    document.querySelector("#carousel__item-3-button").disabled = true;
-    document.querySelector("#carousel__item-4-button").disabled = true;
+  document.querySelector("#carousel__item-1-button").disabled = true;
+  document.querySelector("#carousel__item-2-button").disabled = true;
+  document.querySelector("#carousel__item-3-button").disabled = true;
+  document.querySelector("#carousel__item-4-button").disabled = true;
 
-    slider.removeEventListener("transitionstart", disableAllButtons);
+  slider.removeEventListener("transitionstart", disableAllButtons);
 }
 
-function enableAllButtons(){
-    leftButton.disabled = false;
-    rightButton.disabled = false;
+function enableAllButtons() {
+  leftButton.disabled = false;
+  rightButton.disabled = false;
 
-    document.querySelector("#carousel__item-1-button").disabled = false;
-    document.querySelector("#carousel__item-2-button").disabled = false;
-    document.querySelector("#carousel__item-3-button").disabled = false;
-    document.querySelector("#carousel__item-4-button").disabled = false;
+  document.querySelector("#carousel__item-1-button").disabled = false;
+  document.querySelector("#carousel__item-2-button").disabled = false;
+  document.querySelector("#carousel__item-3-button").disabled = false;
+  document.querySelector("#carousel__item-4-button").disabled = false;
 
-    slider.removeEventListener("transitionend", enableAllButtons);
+  slider.removeEventListener("transitionend", enableAllButtons);
 }
